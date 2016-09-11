22aca1963f24f349aa0a0609167fdd5c91f09772
John Deegan <john.deegan@hotmail.com>
2016-09-11 14:27:19 +0100

    added images and styling

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -6,9 +6,12 @@ var Main = (props) => {
     <div>
       <Nav/>
       <div className="row">
-        <div className="columns medium-6 large-4 small-centered">
-          {props.children}
+        <div className="columns small-12 small-centering">
+          <img className="resize" src="http://static.tumblr.com/79688bd2df69ad7fa707bfa96a18423a/qqqr2nd/Lojn7tqxh/tumblr_static_9qpo5tcm0ycc8w8kwcwsgkc88.png"/>
         </div>
+      </div>
+      <div className="columns small-12 small-centering text-center">
+        <button className="secondary hollow button" href="#">Listen Live</button>
       </div>
     </div>
   );
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -3,13 +3,19 @@ var React = require('react');
 var Nav = React.createClass({
   render: function () {
     return (
-      <div className="top-bar">
+      <div className="top-bar stacked-for-small">
         <div className="top-bar-left">
           <ul className="menu">
             <li className="menu-text">BFM</li>
             <li>Home</li>
             <li>About</li>
             <li>Contact</li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li><input type="search" placeholder="Search"/></li>
+            <li><button type="button" class="button">Search</button></li>
           </ul>
         </div>
       </div>
